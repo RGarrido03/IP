61877c8d8108e82b6ad32cecea7c1bbd70e67d89
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-02-13 19:44:21 +0000

    Create Exercise 7.py

---
+++ Training exam/Exercise 7.py
@@ -0,0 +1,38 @@
+# O ficheiro school1.csv contém as notas dos alunos de uma turma. Cada linha tem o registo de um aluno e cada coluna tem um campo de informação. As colunas são separadas por caracteres TAB. A primeira linha contém um cabeçalho com os títulos dos campos:
+# Numero Nome Curso Regime DataInscricao nota1 nota2 nota3
+# Os ficheiros school2.csv e school3.csv têm as notas dos alunos de outras turmas, com o mesmo formato. Complete o programa abaixo para ler e processar esses ficheiros.
+#
+# a) Complete a função loadFile(fname, lst) para que, dado o nome de um ficheiro com este formato, leia o seu conteúdo e acrescente um tuplo por cada aluno à lista lst. Cada tuplo deve ter os campos (número, nome, nota1, nota2, nota3). Use o método .split('\t') para dividir cada linha e converta as notas e os números para os tipos adequados.
+#
+# b) Crie uma função notaFinal(reg) que, dado um tuplo com o registo de um aluno, devolva a nota final calculada pela média das três notas no registo.
+#
+# c) Crie uma função printPauta(lst) que, dada uma lista com registos de alunos, mostre uma tabela com os números, nomes e notas finais, formatados e alinhados como no exemplo abaixo. O nome deve aparecer centrado, enquanto o número e a nota devem aparecer ajustados à direita.
+#
+# d) Usando estas funções, complete a função main() para ler os ficheiros, ordenar a lista por ordem alfabetica do nome dos alunos e, finalmente, mostrar a pauta.
+
+
+
+# a)
+def loadFile(fname, lst):
+   fileobj = open(fname, "r")
+   fileobj.readline()
+  
+   for line in fileobj:
+      data = line.strip().split("\t")
+      lst.append((data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
+
+
+# b)
+notaFinal = lambda tupl: round((float(tupl[5]) + float(tupl[6]) + float(tupl[7]))/3,1)
+
+
+# c)
+def printPauta(lst):
+   print("{}{}{}".format("Numero", "Nome".center(72), "Nota"))
+   
+   for student in lst:
+      print(f"{str(student[0]):>6s}{student[1].center(72)}{notaFinal(student)}")
+
+
+# d)
+pautaOrdenada = sorted(pauta, key=lambda a:a[1])
