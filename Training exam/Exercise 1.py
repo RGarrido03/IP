01535cb4ed0521e2a3b2c03a8ca4e0a8f96ad750
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-02-13 19:37:13 +0000

    Create Exercise 1.py

--- Training exam/Exercise 1.py
+++ Training exam/Exercise 1.py
@@ -1,1 +1,11 @@
-#To be written.
+# Given integers n and k, return a list 
+# of length n containing 0 1 2 ... k-1 0 1 2 ... k-1 ...
+# n times. 
+
+def repeatSequence(n, k):
+  lst = []
+  
+  for time in range(n):
+     lst += [number for number in range(k)]
+  
+  return lst
